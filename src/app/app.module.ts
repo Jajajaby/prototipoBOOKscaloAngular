455e8b7a9941e8652199c4d19ef0db5af2b8cb54
jajajaby <javieraormeno.l@gmail.com>
2018-08-16 21:40:54 -0300

    aloo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+
+//RUTAS
+import { APP_ROUTING } from './app.routes';
+
+//SERVICIOS
+import { LibrosService } from './services/libros.service';
+
+
+//COMPONENTES
+import { AppComponent } from './app.component';	
+import { BooksComponent } from './components/books/books.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BooksCardComponent } from './components/books-card/books-card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BooksCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    LibrosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/libros.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class LibrosService {
+	
+	constructor() {
+		console.log("Servicio listo para usar jeje")
+	}
+}
